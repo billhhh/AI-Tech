568af5bf29eed6e380cb55180d8e436a0b2fbc42
bill <371570273@qq.com>
2020-10-09 15:34:46 +1030

    prediction

--- Computer_Vision_II/mnist-cnn.py
+++ Computer_Vision_II/mnist-cnn.py
@@ -63,13 +63,12 @@ def model_test(model, device, test_loader):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
+            output = model(data)
             if visual_ON:
                 for i in range(data.shape[0]):
                     plt.imshow(data[i].squeeze().cpu().numpy()), plt.show()
                     print('target', i, ':', target[i].item())
-
-
-            output = model(data)
+                    print('prediction', i, ':', output[i].argmax())
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
