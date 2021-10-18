55f8aead7b9eccf61f28766e1c92c27cf7d81666
Bill Wang <billhhh@users.noreply.github.com>
2021-10-18 22:22:00 +1030

    Update ebook.py

--- prac5/ebook/ebook.py
+++ prac5/ebook/ebook.py
@@ -1,11 +1,15 @@
 from urllib import request
+import nltk
 from nltk import word_tokenize
 
 # url = "http://www.gutenberg.org/cache/epub/174/pg174.txt"
-url = "http://python.org/"
+url = "https://raw.githubusercontent.com/billhhh/AI-Tech/master/prac5/ebook/pg174.txt"
+
+nltk.download('punkt')
 
 response = request.urlopen(url)
-raw = response.read().decode('utf-8')
+# raw = response.read().decode('utf-8')
+raw = response.read().decode('utf-8-sig')
 print('Number of characters: ', len(raw))
 print('First Line: ', raw[:75])
 tokens = word_tokenize(raw)
