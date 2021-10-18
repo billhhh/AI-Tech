38cdb2ba2cf7fb328a2eb3c893294de85b21c06b
Bill Wang <billhhh@users.noreply.github.com>
2021-10-18 22:07:34 +1030

    Update ebook.py

--- prac5/ebook.py
+++ prac5/ebook.py
@@ -2,7 +2,9 @@ from urllib import request
 from nltk import word_tokenize
 
 url = "http://www.gutenberg.org/cache/epub/174/pg174.txt"
-response = request.urlopen(url)
+req = request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'})
+
+response = request.urlopen(req)
 raw = response.read().decode('utf-8')
 print('Number of characters: ', len(raw))
 print('First Line: ', raw[:75])
