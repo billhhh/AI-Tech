8738f0c77c5cdc453f851743800f9199f6343e0a
Bill Wang <billhhh@users.noreply.github.com>
2021-10-18 21:30:05 +1030

    1

--- prac5/main.py
+++ prac5/main.py
@@ -1,9 +1,21 @@
-# ex1
+# ex1 (1)
 
 import re
 
 line = "Matches bat, bet, bit, bot and but Also matches cricket bat, bitter lemon"
 
-matchObj = re.match(r'b[aeiou]t', line)
-if matchObj:
-    print("matchObj.group() : ", matchObj.group())
+pattern = re.compile(r'b[aeiou]t')
+result = pattern.findall(line)
+print(result)
+
+
+# (2)
+line = "batman"
+line2 = "cricket bat"
+
+# anchor our patterns to the beginning and ends of lines using ^ and $ respectively (or \A and \Z for multiline strings)
+pattern = re.compile(r'^b[aeiou]t')
+result = pattern.findall(line)
+print(result)
+result = pattern.findall(line2)
+print(result)
