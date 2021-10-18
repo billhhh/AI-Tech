04e5565230973bc55d96a31274ae570a9ddbd14b
Bill Wang <billhhh@users.noreply.github.com>
2021-10-18 21:33:59 +1030

    Update main.py

--- prac5/main.py
+++ prac5/main.py
@@ -19,3 +19,18 @@ result = pattern.findall(line)
 print(result)
 result = pattern.findall(line2)
 print(result)
+
+
+# (3)
+passwords = ['my_p8ss-w0rd', 'my_p8ss-w0rdmy_p8ss-w0rdmy_p8ss-w0rd' , 'aaaaaaaaaaa', '22222222222', '____________', '$asd123_']
+
+#pattern = re.compile(r"^[a-z0-9_-]{6,18}$")
+pattern = re.compile(r"[a-z0-9_-]{6,18}")
+
+print ("========match/search========")
+for pwd in passwords:
+    t = re.match(pattern,pwd)
+    if (t):
+        print(t.group(0))
+    else:
+        print("Not match \"%s\"" % pwd)
