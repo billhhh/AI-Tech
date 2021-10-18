f072a5bc41192a0884b5a2f65018e29b22af0294
Bill Wang <billhhh@users.noreply.github.com>
2021-10-18 22:26:08 +1030

    Create syntax_tree.py

---
+++ prac5/syntax_tree/syntax_tree.py
@@ -0,0 +1,22 @@
+import nltk
+
+grammar = nltk.CFG.fromstring("""
+S -> NP VP
+PP -> P NP
+NP -> Det N | Det N PP
+VP -> V NP | V NP PP
+Det -> 'a' | 'The' | 'the'
+N -> 'boy' | 'girl' | 'telescope'
+V -> 'sees'
+P -> 'with'
+""")
+
+sent = ['The', 'boy', 'sees', 'the', 'girl', 'with', 'a', 'telescope']
+parser = nltk.ChartParser(grammar)
+for tree in parser.parse(sent):
+    print(tree)
+
+parser = nltk.parse.RecursiveDescentParser(grammar)
+
+for tree in parser.parse(sent):
+    print(tree)
